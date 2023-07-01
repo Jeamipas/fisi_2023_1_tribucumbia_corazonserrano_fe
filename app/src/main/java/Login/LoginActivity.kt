4fc50b5cdb4ca058a9ffb99c29e9be7f0d2c5d0f
Fercho <renzo.chumpitaz@unmsm.edu.pe>
2023-07-01 16:52:59 -0500

    loginferxo

---
+++ app/src/main/java/Login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package Login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.ingresogastos.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
---
+++ app/src/main/java/Login/RegisterActivity.kt
@@ -0,0 +1,12 @@
+package Login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.ingresogastos.R
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
