3571d237710db45889375642e8817f62c8c07afe
Termita7935 <anbeal789@hotmail.com>
2023-07-01 12:29:44 -0500

    PlantillasBrunex

---
+++ app/src/main/java/RegistrarIE/EgresosPopWindowActivity.kt
@@ -0,0 +1,12 @@
+package com.example.ww_ie.Egresos
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.ingresogastos.R
+
+class EgresosPopWindowActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
