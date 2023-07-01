9f2047bd572f594e026367715b0ea2bc45eefa4f
Termita7935 <anbeal789@hotmail.com>
2023-07-01 16:42:04 -0500

    ArreglosViewsIE

--- app/src/main/java/RegistrarIE/IngresosEliminarPopWindowActivity.kt
+++ app/src/main/java/RegistrarIE/IngresosEliminarPopWindowActivity.kt
@@ -4,9 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.ingresogastos.R
 
-class EgresosEliminarPopWindowActivity : AppCompatActivity() {
+class IngresosEliminarPopWindowActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.ie_activity_egresos_eliminar_pop_window)
+        setContentView(R.layout.activity_ingresos_eliminar_pop_window)
     }
 }
