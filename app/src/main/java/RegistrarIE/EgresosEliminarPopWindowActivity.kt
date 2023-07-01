8addf40423d056e9f74bd069b82002b33467e074
Termita7935 <anbeal789@hotmail.com>
2023-07-01 15:46:16 -0500

    PopWindows

---
+++ app/src/main/java/RegistrarIE/EgresosEliminarPopWindowActivity.kt
@@ -0,0 +1,12 @@
+package RegistrarIE
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.ingresogastos.R
+
+class EgresosEliminarPopWindowActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_egresos_eliminar_pop_window)
+    }
+}
