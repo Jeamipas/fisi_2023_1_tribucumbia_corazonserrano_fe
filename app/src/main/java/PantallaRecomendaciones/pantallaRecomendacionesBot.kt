8863c4131df3f35ebd249ead2e1756cc7d173a18
Jp <Jp@gmail.com>
2023-07-01 14:17:20 -0500

    Agregando pantalla recomendaciones inicial

---
+++ app/src/main/java/PantallaRecomendaciones/pantallaRecomendacionesBot.kt
@@ -0,0 +1,12 @@
+package PantallaRecomendaciones
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.ingresogastos.R
+
+class pantallaRecomendacionesBot : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pantalla_recomendaciones_bot)
+    }
+}
