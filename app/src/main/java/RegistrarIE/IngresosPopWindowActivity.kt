ffad51309e12586dac557dafff363ec38aae2566
Termita7935 <anbeal789@hotmail.com>
2023-07-01 17:13:16 -0500

    BrunoHazLosLayouts

---
+++ app/src/main/java/RegistrarIE/IngresosPopWindowActivity.kt
@@ -0,0 +1,12 @@
+package RegistrarIE
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.ingresogastos.R
+
+class IngresosPopWindowActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.ie_activity_ingresos_pop_window)
+    }
+}
