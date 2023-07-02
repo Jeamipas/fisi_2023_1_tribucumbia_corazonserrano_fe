8d5cb91bff7f84588769c053c8233d02aaa29ad4
Termita7935 <anbeal789@hotmail.com>
2023-07-02 17:49:07 -0500

    PseudoFinalFrontIE

--- app/src/main/java/RegistrarIE/EgresosEliminarPopWindowActivity.kt
+++ app/src/main/java/RegistrarIE/EgresosEliminarPopWindowActivity.kt
@@ -1,12 +1,23 @@
 package RegistrarIE
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.AppCompatImageButton
+import com.example.ingresogastos.MainActivity
 import com.example.ingresogastos.R
 
 class EgresosEliminarPopWindowActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.ie_activity_egresos_eliminar_pop_window)
+
+        val botonCancelar = findViewById<AppCompatImageButton>(R.id.botonCancelar)
+        val botonAceptar = findViewById<AppCompatImageButton>(R.id.botonAceptar)
+
+        botonCancelar.setOnClickListener {
+            val intent = Intent(this, EgresosActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/RegistrarIE/EgresosPopWindowActivity.kt
+++ app/src/main/java/RegistrarIE/EgresosPopWindowActivity.kt
@@ -1,10 +1,16 @@
 package RegistrarIE
 
+import android.app.DatePickerDialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 import androidx.appcompat.widget.AppCompatButton
+import androidx.appcompat.widget.AppCompatImageButton
 import com.example.ingresogastos.R
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Locale
 
 class EgresosPopWindowActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,10 +18,46 @@ class EgresosPopWindowActivity : AppCompatActivity() {
         setContentView(R.layout.ie_activity_egresos_pop_window)
 
         val btnAceptar = findViewById<AppCompatButton>(R.id.btnAceptar)
+        val btnFecha = findViewById<AppCompatImageButton>(R.id.imageButtonDate)
+        val fecha = findViewById<EditText>(R.id.fecha)
+
+        var btnFechaSelected = false
+        var fechaSelected = false
 
         btnAceptar.setOnClickListener {
             val intent = Intent(this, EgresosActivity::class.java)
             startActivity(intent)
+        }
+
+        fun checkSelection(){
+            if (btnFechaSelected || fechaSelected) {
+                // Obtener la fecha actual para establecerla como fecha predeterminada en el DatePicker
+                val currentDate = Calendar.getInstance()
+                val year = currentDate.get(Calendar.YEAR)
+                val month = currentDate.get(Calendar.MONTH)
+                val day = currentDate.get(Calendar.DAY_OF_MONTH)
+
+                // Crear un DatePickerDialog para permitir al usuario seleccionar una fecha
+                val datePickerDialog = DatePickerDialog(this, DatePickerDialog.OnDateSetListener { _, selectedYear, selectedMonth, selectedDay ->
+                    val selectedDate = Calendar.getInstance()
+                    selectedDate.set(selectedYear, selectedMonth, selectedDay)
+
+                    val formattedDate = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault()).format(selectedDate.time)
+                    fecha.setText(formattedDate)
+                }, year, month, day)
+
+                datePickerDialog.show()
+            }
+        }
+
+        btnFecha.setOnClickListener {
+            btnFechaSelected = true
+            checkSelection()
+        }
+
+        fecha.setOnClickListener {
+            fechaSelected = true
+            checkSelection()
         }
     }
 }
