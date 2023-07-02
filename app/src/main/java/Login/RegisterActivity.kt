809845b6317eddfdb60227c873c828a1a6a032d2
Fercho <renzo.chumpitaz@unmsm.edu.pe>
2023-07-02 12:53:55 -0500

    alfredmongol

--- app/src/main/java/Login/RegisterActivity.kt
+++ app/src/main/java/Login/RegisterActivity.kt
@@ -6,29 +6,29 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import androidx.appcompat.widget.AppCompatButton
 import com.example.ingresogastos.R
 
 class RegisterActivity : AppCompatActivity() {
 
-    private lateinit var bttnRegistrar: Button
+
     private lateinit var nombreEditText: EditText
     private lateinit var emailEditText: EditText
     private lateinit var passEditText: EditText
     private lateinit var confirmarpassEditText: EditText
-    private lateinit var bttnIniciar: Button
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
-
         nombreEditText = findViewById(R.id.nombre)
         emailEditText = findViewById(R.id.email)
         passEditText = findViewById(R.id.pass)
         confirmarpassEditText = findViewById(R.id.confirmarpass)
-        bttnIniciar = findViewById(R.id.bttniniciarsesion)
-        bttnRegistrar = findViewById(R.id.bttnregistrar)
+        val btnIniciar= findViewById<AppCompatButton>(R.id.bttniniciarsesion)
+        val btnRegistro = findViewById<AppCompatButton>(R.id.bttnregistrar)
 
-        bttnRegistrar.setOnClickListener {
+        btnRegistro.setOnClickListener {
             val nombre = nombreEditText.text.toString()
             val email = emailEditText.text.toString()
             val pass= passEditText.text.toString()
@@ -36,19 +36,17 @@ class RegisterActivity : AppCompatActivity() {
 
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
-
-
             // Aquí puedes agregar la lógica para validar el inicio de sesión
-           /* if (nombre.isNotEmpty() && pass.isNotEmpty() && email.isNotEmpty() && confirmarpass.isNotEmpty()) {
+           if (nombre.isNotEmpty() && pass.isNotEmpty() && email.isNotEmpty() && confirmarpass.isNotEmpty()) {
                 // Iniciar registro exitoso
                 Toast.makeText(this, "Inicio de sesión exitoso", Toast.LENGTH_SHORT).show()
             } else {
                 // Error de registro
                 Toast.makeText(this, "Error: Ingresa el nombre y la contraseña", Toast.LENGTH_SHORT).show()
-            }*/
+            }
         }
 
-        bttnIniciar.setOnClickListener {
+        btnIniciar.setOnClickListener {
             // Aquí puedes agregar la lógica para abrir la pantalla de registro
             // Toast.makeText(this, "Abrir pantalla de registro", Toast.LENGTH_SHORT).show()
             val intent = Intent(this, LoginActivity::class.java)
